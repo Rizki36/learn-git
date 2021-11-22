0dad73293d143a0516790d44380afc6b3be37751
Rizki36 <cahrizki.rf@gmail.com>
2021-11-22 19:11:54 +0700

    add welcome endpoint

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+app.get("/", () => {
+    console.log("Welcome to Learn Git");
+});
 
 app.listen(port, () => {
     console.log("Yeyyy Server Started");
