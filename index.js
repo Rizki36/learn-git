8e2c532bcf5e90651ff42947141f4316b3d1ee01
Rizki36 <cahrizki.rf@gmail.com>
2021-11-22 20:56:42 +0700

    init sequelize

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("learngit", "root", "", {
+    host: "localhost",
+    dialect: "mariadb",
+});
 
 app.get("/", () => {
     console.log("Welcome to Learn Git");
 });
 
+app.get("/sequelize-test-connection", async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+});
+
 app.listen(port, () => {
     console.log("Yeyyy Server Started");
+    console.log(`http://localhost:${port}/`);
 });
